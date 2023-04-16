322e8a49c9f885e6afc217f2254f8b2924e4e652
tautologer <tautologer@users.noreply.github.com>
2023-04-16 00:26:01 -0700

    clean up persistSession logic

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,15 +2,7 @@ import { BskyAgent } from "@atproto/api";
 import { ls } from "./localStorage";
 import { agent, errorToast, settings, successToast, type Settings } from "./stores";
 
-const persistSession = (event: any, sess: any) => {
-  // TODO: this is a bit of a hack, but it works
-  // we can't easily get the current settings from the store because we're outside a component context
-  // and i don't want to drill it down and deal with subscriptions and stuff
-  const rememberMe = ls.get("poastbox.rememberMe", false);
-  if (rememberMe) {
-    ls.set("poastbox.session", sess);
-  }
-};
+const persistSession = (event: any, sess: any) => ls.set("poastbox.session", sess);
 
 export const attemptLogin = async (service: string, handle: string, password: string, rememberMe: boolean) => {
   if (service && handle && password) {
@@ -23,7 +15,7 @@ export const attemptLogin = async (service: string, handle: string, password: st
     // construct agent & attempt login
     const _agent = new BskyAgent({
       service,
-      persistSession,
+      persistSession: rememberMe ? persistSession : undefined,
     });
     try {
       const result = await _agent.login({
@@ -50,7 +42,7 @@ export const attemptResumeSession = async (settings: Settings) => {
   if (session) {
     const _agent = new BskyAgent({
       service: settings.service,
-      persistSession,
+      persistSession: settings.rememberMe ? persistSession : undefined,
     });
     try {
       const { success } = await _agent.resumeSession(session);
